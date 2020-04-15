e218bf5aa8ca89eee40e3a1b77a949d5a2939f3d
xiayuanliu <liu.xiay@husky.neu.edu>
2020-04-15 11:36:55 -0700

    Link pic to page

--- src/RecipeWeb/Error.jsx
+++ src/RecipeWeb/Error.jsx
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 const Error = () => {
     return (
         <div className={styles.body}>
-            <Link className={styles.link_blue} to="/">
+            <Link to="/">
             <img className={styles.img} src={error} alt="404" title="404"></img>
         
 			</Link>
